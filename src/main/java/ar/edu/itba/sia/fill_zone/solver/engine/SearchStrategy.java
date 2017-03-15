2e1088ae1691d6e33134942418afa73cf7533fd8
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-03-15 16:36:56 -0300

    Add the general problem solver classes

---
+++ src/main/java/ar/edu/itba/sia/fill_zone/solver/engine/SearchStrategy.java
@@ -0,0 +1,5 @@
+package ar.edu.itba.sia.fill_zone.solver.engine;
+
+public enum SearchStrategy {
+	BFS, DFS
+}
---
+++ src/main/java/ar/edu/itba/sia/fill_zone/solver/interfaces/GPSState.java
@@ -0,0 +1,16 @@
+package ar.edu.itba.sia.fill_zone.solver.interfaces;
+
+/**
+ * GPSState interface.
+ */
+public interface GPSState {
+
+	/**
+	 * Compares self to another state to determine whether they are the same or not.
+	 *
+	 * @param state The state to compare to.
+	 * @return true if self is the same as the state given, false if they are different.
+	 */
+	boolean equals(Object state);
+
+}
