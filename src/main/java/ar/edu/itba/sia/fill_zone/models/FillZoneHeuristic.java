68935a5338ab068586c3e0b97ac43d698b22ca31
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-03-22 18:18:40 -0300

    Javadoc to heuristics enum

--- src/main/java/ar/edu/itba/sia/fill_zone/models/FillZoneHeuristic.java
+++ src/main/java/ar/edu/itba/sia/fill_zone/models/FillZoneHeuristic.java
@@ -3,57 +3,52 @@ package ar.edu.itba.sia.fill_zone.models;
 import ar.edu.itba.sia.fill_zone.models.interfaces.Heuristic;
 import ar.edu.itba.sia.fill_zone.solver.api.GPSState;
 
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+public enum FillZoneHeuristic implements Heuristic {
 
-public enum FillZoneHeuristic implements Heuristic{
+	/**
+	 * Amount of colors remaining in the board.
+	 */
+	REMAINING_COLORS() {
+		@Override
+		public Integer getHValue(GPSState state) {
+			final FillZoneState fillZoneState = (FillZoneState) state;
+			final int[] colorRegister = fillZoneState.getBoard().getAmountOfEachColor();
 
+			int c = 0;
+			for (int i = 0; i < colorRegister.length; i++) {
+				if (colorRegister[i] > 0)
+					c++;
+			}
+			return c;
+		}
+	},
 
-    REMAINING_COLORS(){
-        @Override
-        public Integer getHValue(GPSState state) {
-            final FillZoneState fillZoneState = (FillZoneState) state;
-            final int[] colorRegister = fillZoneState.getBoard().getColorRegister();
+	//TODO
+	/**
+	 * Amount of groups of colors in the board.
+	 */
+	REMAINING_GROUPS() {
+		@Override
+		public Integer getHValue(GPSState state) {
+			final FillZoneState fillZoneState = (FillZoneState) state;
+			final Board board = fillZoneState.getBoard();
 
-            int c = 0;
-            for(int i = 0; i < colorRegister.length; i++){
-                if(colorRegister[i] > 0)
-                    c++;
-            }
-            return c;
-        }
-    },
+			boolean[][] groupedBoxes = new boolean[board.getRows()][board.getColumns()];
+			int c = 0;
 
-    //TODO
-    REMAINING_GROUPS(){
-        @Override
-        public Integer getHValue(GPSState state) {
-            final FillZoneState fillZoneState = (FillZoneState) state;
-            final Board board = fillZoneState.getBoard();
+			for (int i = 0; i < board.getColumns(); i++) {
+				for (int j = 0; j < board.getRows(); j++) {
+					if (!groupedBoxes[i][j]) {
+						c++;
+						groupedBoxes[i][j] = true;
+						//TODO funcion para agregar coordinadas true a los grouped boxes
+					}
+				}
+			}
 
-            boolean[][] groupedBoxes = new boolean[board.getRows()][board.getColumns()];
-            int c = 0;
-
-            for(int i = 0; i < board.getColumns(); i++){
-                for(int j=0; j < board.getRows(); j++){
-                    if(!groupedBoxes[i][j]){
-                        c++;
-                        groupedBoxes[i][j] = true;
-                        //TODO funcion para agregar coordinadas true a los grouped boxes
-                    }
-                }
-            }
-
-            return c;
-        }
-    },
-
-    ;
-
-
-
+			return c;
+		}
+	},;
 
 
 }
