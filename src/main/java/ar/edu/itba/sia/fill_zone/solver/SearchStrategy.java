47d89bfb7880d4cd6c68411e7329aee7b26a5cc9
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-03-28 04:13:03 -0300

    Add Main Class
    - Include plugin in pom

--- src/main/java/ar/edu/itba/sia/fill_zone/solver/SearchStrategy.java
+++ src/main/java/ar/edu/itba/sia/fill_zone/solver/SearchStrategy.java
@@ -1,5 +1,9 @@
 package ar.edu.itba.sia.fill_zone.solver;
 
 public enum SearchStrategy {
-	BFS, DFS, IDDFS, GREEDY, ASTAR
+	BFS, DFS, IDDFS, GREEDY, ASTAR;
+
+	public static SearchStrategy fromString(String name) {
+		return SearchStrategy.valueOf(name.toUpperCase());
+	}
 }
