7ebd7cd60a7a62ffc67cd0654b98e98848bd5829
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-03-28 03:27:28 -0300

    Refactor FillZoneState
    - Add javadoc
    - Add finals
    - Change equals to use Board's equals method
    - Add hashcode method

--- src/main/java/ar/edu/itba/sia/fill_zone/models/FillZoneState.java
+++ src/main/java/ar/edu/itba/sia/fill_zone/models/FillZoneState.java
@@ -3,33 +3,63 @@ package ar.edu.itba.sia.fill_zone.models;
 
 import ar.edu.itba.sia.fill_zone.solver.api.GPSState;
 
-import java.util.Arrays;
-
+/**
+ * A fill zone state.
+ */
 public class FillZoneState implements GPSState {
 
-	private Board board;
+	/**
+	 * The board according to the state.
+	 */
+	private final Board board;
 
-	private int moves;
+	/**
+	 * The amount of moves done when this state is reached.
+	 */
+	private final int moves;
 
+	/**
+	 * Constructor.
+	 *
+	 * @param board The board according to the state.
+	 * @param moves The amount of moves done when this state is reached.
+	 */
 	public FillZoneState(Board board, int moves) {
 		this.board = board;
 		this.moves = moves;
 	}
 
+
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
+		if (!(o instanceof FillZoneState)) return false;
 
 		FillZoneState that = (FillZoneState) o;
 
-		return Arrays.deepEquals(board.getMatrix(), that.getBoard().getMatrix());
+		return board.equals(that.board);
+
 	}
 
+	@Override
+	public int hashCode() {
+		return board.hashCode();
+	}
+
+	/**
+	 * Board getter.
+	 *
+	 * @return The board.
+	 */
 	public Board getBoard() {
 		return board;
 	}
 
+	/**
+	 * Moves getter.
+	 *
+	 * @return The moves.
+	 */
 	public int getMoves() {
 		return moves;
 	}
