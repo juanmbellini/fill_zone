70e68bc711e5b8a2b7482945f522ec48ee5a2644
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-04-04 01:39:57 -0300

    Add method to BoardReader to read from board from resources

--- src/main/java/ar/edu/itba/sia/fill_zone/io/BoardReader.java
+++ src/main/java/ar/edu/itba/sia/fill_zone/io/BoardReader.java
@@ -3,13 +3,16 @@ package ar.edu.itba.sia.fill_zone.io;
 import ar.edu.itba.sia.fill_zone.io.exception.WrongBoardFormatException;
 import ar.edu.itba.sia.fill_zone.models.Board;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 /**
  * Class implementing a method to read boards from a text file.
@@ -34,9 +37,31 @@ public class BoardReader {
 	 * @throws IOException               If an I/O error occurs while reading the file.
 	 * @throws WrongBoardFormatException If file's format is incorrect.
 	 */
-	public static Board readBoard(String path) throws IOException, WrongBoardFormatException {
-		final Stream<String> fileStream = Files.lines(Paths.get(path));
-		List<String> lines = fileStream.collect(Collectors.toList());
+	public static Board readBoardFromFile(String path) throws IOException, WrongBoardFormatException {
+		return readBoard(Files.lines(Paths.get(path)).collect(Collectors.toList()));
+	}
+
+	/**
+	 * Load a board according to the given {@code boardNumber} from resources.
+	 *
+	 * @param boardNumber The board number to load.
+	 * @return The loaded board
+	 * @throws IOException If an I/O error occurs while reading the file.
+	 */
+	public static Board readResourceBoard(int boardNumber) throws IOException {
+		InputStream in = BoardReader.class.getResourceAsStream("/Board" + boardNumber + ".brd");
+		return readBoard(createStringListFromInputStream(in));
+	}
+
+
+	/**
+	 * Creates a board from the given list of lines.
+	 *
+	 * @param lines The list of {@link String} with the lines representing the board.
+	 * @return The created board.
+	 * @throws IOException If an I/O error occurs while reading the file.
+	 */
+	private static Board readBoard(List<String> lines) throws IOException {
 		final int rows = Integer.parseInt(lines.remove(0));
 		final int columns = Integer.parseInt(lines.remove(0));
 		final int colors = Integer.parseInt(lines.remove(0));
@@ -71,4 +96,24 @@ public class BoardReader {
 		return new Board(rows, columns, colors, matrix);
 	}
 
+	/**
+	 * Creates a {@link List} of {@link String} based on the given {@link InputStream}.
+	 *
+	 * @param in The input stream.
+	 * @return The list of strings read as lines from the input stream.
+	 * @throws IOException If an I/O error occurs while reading the input stream.
+	 */
+	private static List<String> createStringListFromInputStream(InputStream in) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		List<String> lines = new LinkedList<>();
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			lines.add(line);
+		}
+		return lines;
+
+	}
+
 }
+
+
